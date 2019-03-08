7c62680006f9c754d5ac0def802bd0d0915da40a
pinnuli <1192161354@qq.com>
2019-03-08 18:18:50 +0800

    34题

---
+++ SwordFingerOffer/src/main/java/the34_二叉树中和为某一值的路径/TreeNode.java
@@ -0,0 +1,17 @@
+package the34_二叉树中和为某一值的路径;
+
+/**
+ * @author: pinnuli
+ * @date: 2019-02-27
+ */
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
